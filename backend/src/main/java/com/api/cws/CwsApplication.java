c3c90f15c179eecbb694b1f45acd96d9f502116f
IZEX_WEB <http://192.168.100.91:3000/WEB/izexWTM>
2022-09-15 16:21:59 +0900

    # Author        : 소현태 # Date          : 2022-09-15 # DB            : # Description   : [공통] 최초 커밋.

---
+++ backend/src/main/java/com/api/cws/CwsApplication.java
@@ -0,0 +1,13 @@
+package com.api.cws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CwsApplication.class, args);
+	}
+
+}
