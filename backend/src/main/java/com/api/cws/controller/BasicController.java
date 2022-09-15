680a7a5413beaee2a8609fc7a41eb16dee5f8587
IZEX_WEB <http://192.168.100.91:3000/WEB/izexWTM>
2022-09-15 18:30:37 +0900

    # Author : 소현태 # Date          : 2022-09-15 # DB            : # Description   : backend setting

---
+++ backend/src/main/java/com/api/cws/controller/BasicController.java
@@ -0,0 +1,7 @@
+package com.api.cws.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BasicController {
+}
---
+++ backend/src/main/java/com/api/cws/controller/graphql/GraphqlController.java
@@ -0,0 +1,7 @@
+package com.api.cws.controller.graphql;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GraphqlController {
+}
