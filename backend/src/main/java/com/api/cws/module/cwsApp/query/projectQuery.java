3a8cb938c3143968a3e8d5027885a7a3e5c39969
hyuntae <33915266+sohyuntae@users.noreply.github.com>
2022-12-19 20:29:59 +0900

    # Author : 소현태 # Date   : 2022-12-19 # DB     : # Description : 1. 요청 프로젝트 관련 조회 및 저장 api 작업 (진행중)  - domain 추가 및 service 쪽 구현 진행중

---
+++ backend/src/main/java/com/api/cws/module/cwsApp/query/projectQuery.java
@@ -0,0 +1,32 @@
+package com.api.cws.module.cwsApp.query;
+
+import com.api.cws.module.cwsApp.repository.ProjectService;
+import com.api.cws.module.cwsApp.service.*;
+import com.dgs.dgsframework.types.addProjectInfo;
+import com.dgs.dgsframework.types.projectInfo;
+import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsMutation;
+import com.netflix.graphql.dgs.DgsQuery;
+import com.netflix.graphql.dgs.InputArgument;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@DgsComponent
+@RequiredArgsConstructor
+public class projectQuery {
+    private final ProjectService projectService;
+
+    @DgsQuery
+    @Transactional(readOnly = true)
+    public List<projectInfo> getProjectInfo(@InputArgument List<Long> projectKey) {
+        return projectService.getProjectInfo(projectKey);
+    }
+
+    @DgsMutation
+    @Transactional
+    public String setProjectInfo(@InputArgument addProjectInfo addProjectInfo) {
+        return projectService.setProjectInfo(addProjectInfo);
+    }
+}
