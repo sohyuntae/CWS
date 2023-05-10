27c4f7316de43527a73b87117e695d5468ad144e
hyuntae <sotae69@naver.com>
2023-05-10 17:01:23 +0900

    open api 테스트

--- backend/src/test/java/com/api/cws/CwsApplicationTests.java
+++ backend/src/test/java/com/api/cws/CwsApplicationTests.java
@@ -1,6 +1,36 @@
 package com.api.cws;
 
-import org.springframework.boot.test.context.SpringBootTest;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.io.BufferedReader;
+import java.io.IOException;
 
-class CwsApplicationTests {
+public class CwsApplicationTests {
+    public static void main(String[] args) throws IOException {
+        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getRestDeInfo"); /*URL*/
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=5ZJlJzGxzZovjJLjzAtmQVXMjmGiJWECA2kYFsuGhv83tou11zPlJ1q%2BJrtnubNX3hzFxoncFvVbyM95PM1fmg%3D%3D"); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("solYear","UTF-8") + "=" + URLEncoder.encode("2023", "UTF-8")); /*연*/
+        urlBuilder.append("&" + URLEncoder.encode("solMonth","UTF-8") + "=" + URLEncoder.encode("02", "UTF-8")); /*월*/
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        System.out.println("Response code: " + conn.getResponseCode());
+        BufferedReader rd;
+        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();
+        System.out.println(sb.toString());
+    }
 }
